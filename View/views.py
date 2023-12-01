20e49436de96209402c53a6d558cfba4588961d3
Wanderson <129337663+wandersonwendel@users.noreply.github.com>
2023-12-01 10:07:28 -0300

    Update views.py

--- View/views.py
+++ View/views.py
@@ -1,26 +1,29 @@
-""" Esse arquivo é responsável pelas views """
-
-
-class PassageiroView:
-    """ Classe View do passageiro """
+class VeiculoView:
+    """ Classe View do Veículo """
     def mostrar_mensagem(self, mensagem):
-        """ Método View do passageiro para mostrar mensagem """
+        """ Método View do Veículo para mostrar mensagem """
         print(mensagem)
 
 class CartaoView:
     """ Classe View do Cartão """
     def mostrar_mensagem(self, mensagem):
-        """ Método View do cartao para mostrar mensagem """
+        """ Método View do Cartão para mostrar mensagem """
+        print(mensagem)
+
+class PassageiroView:
+    """ Classe View do Passageiro """
+    def mostrar_mensagem(self, mensagem):
+        """ Método View do Passageiro para mostrar mensagem """
         print(mensagem)
 
 class MototaxiView:
     """ Classe View do Mototaxi """
     def mostrar_mensagem(self, mensagem):
-        """ Método View do mototaxi para mostrar mensagem """
+        """ Método View do Mototaxi para mostrar mensagem """
         print(mensagem)
 
 class UsuarioView:
     """ Classe View do Usuario """
     def mostrar_mensagem(self, mensagem):
-        """ Método View do usuario para mostrar mensagem """
+        """ Método View do Usuario para mostrar mensagem """
         print(mensagem)
